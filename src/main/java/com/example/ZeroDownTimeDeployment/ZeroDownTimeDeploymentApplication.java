5119ba47eb4db4ef052ec4299c6da1ff37a3465b
JuHyeonPark <parkjh080@naver.com>
2024-09-02 21:47:42 +0900

    init: 프로젝트 생성
    프로젝트 생성

---
+++ src/main/java/com/example/ZeroDownTimeDeployment/ZeroDownTimeDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZeroDownTimeDeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroDownTimeDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroDownTimeDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ZeroDownTimeDeployment/ZeroDownTimeDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZeroDownTimeDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDownTimeDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
